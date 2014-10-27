0782323999c91449fe3c2f962929d94fb1513944
Chris Spearing <chris@spearing.me>
2014-10-27 23:59:29 +0000

    Added draggable example

--- js/app.js
+++ js/app.js
@@ -12,7 +12,8 @@ var bankFriendsApp = angular.module('bankFriendsApp', [
   'elasticsearch',
   'ui.bootstrap',
   'ngCookies',
-  'angular-loading-bar'
+  'angular-loading-bar',
+  'drag'
 ]);
 
 //configure the routing
@@ -37,3 +38,38 @@ bankFriendsApp.config(['$routeProvider',
         redirectTo: '/home'
       });
   }]);
+
+angular.module('drag', []).
+  directive('draggable', function($document) {
+    return function(scope, element, attr) {
+      var startX = 0, startY = 0, x = 0, y = 0;
+      element.css({
+       position: 'relative',
+      
+       cursor: 'pointer',
+       display: 'block'
+      });
+      element.on('mousedown', function(event) {
+        // Prevent default dragging of selected content
+        event.preventDefault();
+        startX = event.screenX - x;
+        startY = event.screenY - y;
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+      });
+
+      function mousemove(event) {
+        y = event.screenY - startY;
+        x = event.screenX - startX;
+        element.css({
+          top: y + 'px',
+          left:  x + 'px'
+        });
+      }
+
+      function mouseup() {
+        $document.off('mousemove', mousemove);
+        $document.off('mouseup', mouseup);
+      }
+    };
+  });
